376b3d7a0780d8cc74a7211911fb5eedd8e553f3
Tim <tim@reach-x.com>
2019-10-20 21:43:30 -0700

    Residentials PK set to fillable.

--- app/Residential.php
+++ app/Residential.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Residential extends Model
-{
-    //
+class Residential extends Model {
+    protected $fillable = ['listingkeynumeric'];
 }
